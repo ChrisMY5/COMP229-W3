b12a99814ecaf24971faf4a06d4f4c5198144b40
Chris MY Oh <carlsearin@hotmail.com>
2021-10-02 01:54:18 -0400

    comments added

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules')));
+app.use(express.static(path.join(__dirname, 'node_modules'))); // add node_module for bootstrap
 
+// router starting point 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,31 @@
-
+/* Man Young Oh / 301161472 / COMP229-005 */
 
 /* load the framework and all routes defining  */
-
 let express = require('express');
 let router = express.Router();
 
-/* GET home page. */
+/* route to localhost and activate to load 'Home' page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home', page: 'home' });
 });
-/* GET home page. */
+/* route to localhost and activate to load 'Home' page. */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home', page: 'home'  });
 });
-/* GET About page. */
+/* route to localhost and activate to load 'About' page. */
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About Me', page: 'about'  });
 });
-/* GET Products page. */
+/* route to localhost and activate to load 'Projects' page. */
 router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects', page: 'project'  });
 });
-/* GET Services page. */
+/* route to localhost and activate to load 'Services' page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services', page: 'services'  });
 });
-/* route to localhost:3000/Contact to open 'Contact' page. */
+/* route to localhost and activate to load 'Contact' page. */
 router.get('/Contact', function(req, res, next) {
   res.render('index', { title: 'Contact', page: 'contact'  });
 });
-
 module.exports = router;
