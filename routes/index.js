23c5756e56d665180f28d95766c7e6da6d4258ff
Chris MY Oh <carlsearin@hotmail.com>
2021-10-01 23:25:50 -0400

    intiate partial contents

--- routes/index.js
+++ routes/index.js
@@ -7,27 +7,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home' });
 });
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home', page: 'home'  });
 });
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('index', { title: 'About Me', page: 'about'  });
 });
 /* GET Products page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('index', { title: 'Projects', page: 'project'  });
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: 'Services', page: 'services'  });
 });
 /* route to localhost:3000/Contact to open 'Contact' page. */
 router.get('/Contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: 'Contact', page: 'contact'  });
 });
 
 module.exports = router;
