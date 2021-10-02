76bb052e226e311851dcc544379b580963a3fec0
Chris MY Oh <carlsearin@hotmail.com>
2021-10-01 22:48:51 -0400

    variable declaration change

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
-var express = require('express');
-var router = express.Router();
+
+
+/* load the framework and all routes defining  */
+
+let express = require('express');
+let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
